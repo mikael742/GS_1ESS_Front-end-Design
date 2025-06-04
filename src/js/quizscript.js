e9e2697bda1f24fd3eb558f39dfbd91b0f5ebcd6
mikael742 <santanamikael742@gmail.com>
2025-06-03 21:12:15 -0300

    Atualização Menu Hámmburguer

--- src/js/quizscript.js
+++ src/js/quizscript.js
@@ -162,3 +162,20 @@ nextButton.addEventListener("click", handleNextButton);
 restartButton.addEventListener("click", startQuiz);
 
 startQuiz();
+
+//Hambúrguer
+
+const btnHamburguer = document.getElementById('btnHamburguer');
+const nav = document.querySelector('header nav');
+
+btnHamburguer.addEventListener('click', () => {
+  const aberto = nav.classList.toggle('menu-aberto'); // adiciona/remove a classe e retorna se está aberto
+
+  if (aberto) {
+    btnHamburguer.innerHTML = '&times;'; // mostra X
+    btnHamburguer.setAttribute('aria-label', 'Fechar menu');
+  } else {
+    btnHamburguer.innerHTML = '&#9776;'; // mostra hambúrguer
+    btnHamburguer.setAttribute('aria-label', 'Abrir menu');
+  }
+});
